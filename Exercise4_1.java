3580b4d71acf08a277ce1e5f658ccf209f236a80
enesdemir72 <250541047@firat.edu.tr>
2025-11-29 18:25:55 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,40 @@
 public class Exercise4_1 {
 
+    /**
+     * Prints the date in American format: Day, Month Date, Year
+     * Example: Saturday, November 29, 2025
+     */
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        // American format: Day, Month Date, Year
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
+    /**
+     * Prints the date in European format: Day Date Month Year
+     * Example: Saturday 29 November 2025
+     */
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        // European format: Day Date Month Year
+        System.out.println(day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        // Test variables using a current date example
+        String testDay = "Saturday";
+        int testDate = 29;
+        String testMonth = "November";
+        int testYear = 2025;
+
+        System.out.println("--- Exercise 4.1 - Date Printing Methods ---");
+
+        // Calling American format method
+        System.out.print("American Format: ");
+        printAmerican(testDay, testDate, testMonth, testYear);
+
+        // Calling European format method
+        System.out.print("European Format: ");
+        printEuropean(testDay, testDate, testMonth, testYear);
+        
+        System.out.println("------------------------------------------");
     }
 }
