4d36de766f923616e6eac7ada2d3978c8bdeb88f
enesdemir72 <250541047@firat.edu.tr>
2025-11-29 18:30:10 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -2,9 +2,9 @@ public class Exercise4_5 {
 
     public static void zoop(String fred, int bob) {
         System.out.println(fred);
-        if (bob == 5) {
+        if (bob == 5) { // 1. cagri: bob=5 (True)
             ping("not ");
-        } else {
+        } else { // 2. cagri: bob=4 (False)
             System.out.println("!");
         }
     }
@@ -12,13 +12,19 @@ public class Exercise4_5 {
     public static void main(String[] args) {
         int bizz = 5;
         int buzz = 2;
-        zoop("just for", bizz);
-        clink(2 * buzz);
+        
+        // 1. zoop cagrisi: fred="just for", bob=5
+        zoop("just for", bizz); 
+        
+        // clink cagrisi: fork = 2 * 2 = 4
+        clink(2 * buzz); 
     }
 
     public static void clink(int fork) {
         System.out.print("It's ");
-        zoop("breakfast ", fork);
+        
+        // 2. zoop cagrisi: fred="breakfast ", bob=4
+        zoop("breakfast ", fork); 
     }
 
     public static void ping(String strangStrung) {
